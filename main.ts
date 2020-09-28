03a8da312fce84f9231751f8d04cc62d8618797f
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-09-28 14:18:47 -0400

    added a changing variable and a comment

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+// Created By: Mariam Abugamga
+// Date: sept, 28, 2020
+// Program changes a variable 
+// 
+let hungryness = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        hungryness = hungryness + 1
+        basic.showNumber(hungryness)
+    } else if (input.buttonIsPressed(Button.B)) {
+        hungryness = 0
+        basic.showNumber(hungryness)
+    }
 })
